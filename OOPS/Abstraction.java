3dfd16b0038c87da5400a0a7e2647ebd1564d165
rapoluprashanth <rapoluprashanth123@gmail.com>
2026-05-24 20:07:35 +0530

    first commit

---
+++ OOPS/Abstraction.java
@@ -0,0 +1,67 @@
+package OOPS;
+interface Car{
+    void startEngine();
+    void shiftGear(int gear);
+    void accelerate();
+    void brake();
+    void stopEngine();
+}
+
+class MyCar implements Car{
+    String brand;
+    String model;
+    boolean isEngineOn = false;
+    int currentSpeed = 0;
+    int currentGear = 0;
+
+    MyCar(String brand,String model){
+      this.brand=brand;
+      this.model=model;
+    } 
+
+    public void startEngine(){
+      isEngineOn=true;
+      System.out.println(brand + " " + model + " : Engine starts with a roar!");
+    }
+
+    public void shiftGear(int gear) {
+        if (!isEngineOn) {
+            System.out.println(brand + " " + model + " : Engine is off! Cannot Shift Gear.");
+            return;
+        }
+        this.currentGear = gear;
+        System.out.println(brand + " " + model + " : Shifted to gear " + currentGear);
+    }
+
+    public void accelerate() {
+        if (!isEngineOn) {
+            System.out.println(brand + " " + model + " : Engine is off! Cannot accelerate.");
+            return;
+        }
+        currentSpeed += 20;
+        System.out.println(brand + " " + model + " : Accelerating to " + currentSpeed + " km/h");
+    }
+
+    public void brake() {
+        currentSpeed -= 20;
+        if (currentSpeed < 0) currentSpeed = 0;
+        System.out.println(brand + " " + model + " : Braking! Speed is now " + currentSpeed + " km/h");
+    }
+
+    public void stopEngine() {
+        isEngineOn = false;
+        currentGear = 0;
+        currentSpeed = 0;
+        System.out.println(brand + " " + model + " : Engine turned off.");
+    }
+}
+public class Abstraction{
+    public static void main(String[] args) {
+        MyCar obj=new MyCar("Suzuki","alto");
+        obj.startEngine();
+        obj.shiftGear(5);
+        obj.accelerate();
+        obj.brake();
+        obj.stopEngine();
+    }
+}
