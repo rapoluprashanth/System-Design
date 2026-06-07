4217b1e3267ceb603b3b4e17f1a1b2b02fed44ba
rapoluprashanth <rapoluprashanth123@gmail.com>
2026-06-07 20:39:18 +0530

    SRPViolated implementation

---
+++ SOLID_DESIGN_PRINCIPLES/SingleResponsibilityPrinciple/SRPFollowed.java
@@ -0,0 +1,7 @@
+package SOLID_DESIGN_PRINCIPLES.SingleResponsibilityPrinciple;
+
+public class SRPFollowed {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ SOLID_DESIGN_PRINCIPLES/SingleResponsibilityPrinciple/SRPViolated.java
@@ -0,0 +1,62 @@
+package SOLID_DESIGN_PRINCIPLES.SingleResponsibilityPrinciple;
+
+import java.util.*;
+
+class Product {
+    protected String name;
+    protected double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+// Violating SRP: ShoppingCart is handling multiple responsibilities
+class ShoppingCart {
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    // 1. Calculates total price in cart.
+    public double calculateTotalPrice() {
+        double totalPrice = 0;
+        for (Product p : products) {
+            totalPrice += p.price;
+        }
+        return totalPrice;
+    }
+
+    // 2. Violating SRP - Prints invoice (Should be in a separate class)
+    public void printInvoice() {
+        System.out.println("Shopping Cart Invoice:");
+        for (Product p : products) {
+            System.out.println(p.name + " " + p.price);
+        }
+        System.out.println("The TotalPrice:" + calculateTotalPrice());
+    }
+
+    // 3. Violating SRP - Saves to DB (Should be in a separate class)
+    public void saveToDatabase() {
+        System.out.println("Saving shopping cart to database...");
+    }
+}
+
+public class SRPViolated {
+    public static void main(String[] args) {
+        ShoppingCart obj = new ShoppingCart();
+
+        obj.addProduct(new Product("Laptop", 50000.0));
+        obj.addProduct(new Product("Mouse", 1000.0));
+        obj.addProduct(new Product("Bag", 2000.0));
+
+        obj.printInvoice();
+        obj.saveToDatabase();
+    }
+}
