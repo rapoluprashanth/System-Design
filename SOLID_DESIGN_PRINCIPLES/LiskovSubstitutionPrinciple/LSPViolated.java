aacdf0a654ed8fd6e094743a217a88361601bcaa
rapoluprashanth <rapoluprashanth123@gmail.com>
2026-06-15 00:02:47 +0530

    LSPViolated implementation

---
+++ SOLID_DESIGN_PRINCIPLES/LiskovSubstitutionPrinciple/LSPViolated.java
@@ -0,0 +1,97 @@
+package SOLID_DESIGN_PRINCIPLES.LiskovSubstitutionPrinciple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Account {
+    public abstract void withdraw(double amount);
+
+    public abstract void deposit(double amount);
+}
+
+class SavingAccount extends Account {
+
+    private double balance;
+
+    public SavingAccount() {
+        balance = 0;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount + " from Savings Account. New Balance: " + balance);
+        } else {
+            System.out.println("Insufficient funds in Savings Account!");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount + " in Savings Account. New Balance: " + balance);
+    }
+}
+
+class CurrentAccount extends Account {
+    private double balance;
+
+    public CurrentAccount() {
+        balance = 0;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount + " from Savings Account. New Balance: " + balance);
+        } else {
+            System.out.println("Insufficient funds in Savings Account!");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount + " in Savings Account. New Balance: " + balance);
+    }
+}
+
+class FixedTermAccount extends Account {
+    private double balance;
+
+    public FixedTermAccount() {
+        balance = 0;
+    }
+
+    public void withdraw(double amount) {
+        throw new UnsupportedOperationException("Withdrawal not allowed in Fixed Term Account!");
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount + " in Savings Account. New Balance: " + balance);
+    }
+}
+
+class Client {
+    private List<Account> accounts;
+
+    public Client(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void processTransaction() {
+
+    }
+
+}
+
+public class LSPViolated {
+    public static void main(String[] args) {
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(new SavingAccount());
+        accounts.add(new CurrentAccount());
+        accounts.add(new FixedTermAccount());
+
+        Client obj = new Client(accounts);
+        obj.processTransaction();
+    }
+}
