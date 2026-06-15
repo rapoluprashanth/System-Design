7f9ec535570818530000a9c9860b1c4bb69e2ff7
rapoluprashanth <rapoluprashanth123@gmail.com>
2026-06-15 20:06:52 +0530

    LSPFollowedWrongly implementation

---
+++ SOLID_DESIGN_PRINCIPLES/LiskovSubstitutionPrinciple/LSPFollowedWrongly.java
@@ -0,0 +1,111 @@
+package SOLID_DESIGN_PRINCIPLES.LiskovSubstitutionPrinciple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// violation of SRP and OCP
+abstract class Account {
+    public abstract void withdraw(double amount);
+
+    public abstract void deposit(double amount);
+}
+
+class SavingAccount extends Account {
+
+    private double balance;
+
+    public SavingAccount() {
+        balance = 0;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount + " from Savings Account. New Balance: " + balance);
+        } else {
+            System.out.println("Insufficient funds in Savings Account!");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount + " in Savings Account. New Balance: " + balance);
+    }
+}
+
+class CurrentAccount extends Account {
+    private double balance;
+
+    public CurrentAccount() {
+        balance = 0;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount + " from Current Account. New Balance: " + balance);
+        } else {
+            System.out.println("Insufficient funds in Savings Account!");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount + " in Current Account. New Balance: " + balance);
+    }
+}
+
+class FixedTermAccount extends Account {
+    private double balance;
+
+    public FixedTermAccount() {
+        balance = 0;
+    }
+
+    public void withdraw(double amount) {
+        throw new UnsupportedOperationException("Withdrawal not allowed in Fixed Term Account!");
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount + " in Fixed Term Account. New Balance: " + balance);
+    }
+}
+
+class Client {
+    private List<Account> accounts;
+
+    public Client(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void processTransaction() {
+        for (Account acc : accounts) {
+            acc.deposit(1000); // All accounts allow deposits
+
+            // checking if account is FixedTermAccount and yes skipping withdrawal
+            if (acc instanceof FixedTermAccount)
+                System.out.println("Skipping withdrawal for Fixed Term Account.");
+            else {
+                try {
+                    acc.withdraw(500);
+                } catch (UnsupportedOperationException e) {
+                    System.err.println(e);
+                }
+            }
+        }
+    }
+
+}
+
+public class LSPFollowedWrongly {
+    public static void main(String[] args) {
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(new SavingAccount());
+        accounts.add(new CurrentAccount());
+        accounts.add(new FixedTermAccount());
+
+        Client obj = new Client(accounts);
+        obj.processTransaction();
+    }
+}
